da5a5d170f5262e4f5b6cee4b38524070ef9bf24
Chenoa <cowmanifestation@gmail.com>
2009-03-03 13:44:12 -0500

    Adding appointment book

--- appointmentbook.rb
+++ appointmentbook.rb
@@ -1,9 +1,46 @@
-class AppointmentBook
-	
-	require "date"
-	
-	def initialize
-		@appointment_book = Hash.new("No appointments.")
-	end
-	
-	
+require "date"
+require "pstore"
+
+class Schedule
+
+  include Enumerable
+
+  def initialize(file="schedule.store")
+    @store = PStore.new(file)
+  end
+
+  def [](date)
+    @store.transaction do 
+      events_for(date)
+    end
+  end
+
+  def event(date, text)
+    @store.transaction do
+      events_for(date) << text
+    end
+  end
+
+  def each
+    @store.transaction do
+      @store.roots.each { |date| yield([date, @store[date]]) }
+    end
+  end
+
+  def to_s
+    map do |(date, appointments)|
+      date.strftime("%m/%d/%Y") + " (#{appointments.length})"
+    end.join("\n")
+  end
+
+  private
+
+  def events_for(date)
+    @store[to_date(date)] ||= []
+  end
+
+  def to_date(str)
+    Date.strptime(str, "%m/%d/%Y")
+  end
+
+end
